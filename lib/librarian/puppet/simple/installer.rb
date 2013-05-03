38a89828cb3c5668adaf377dec600b8a1da8e8c6
Rafael Rosa <rrosa@iweb.com>
2013-05-03 14:31:15 -0400

    Simplified verbose output

--- lib/librarian/puppet/simple/installer.rb
+++ lib/librarian/puppet/simple/installer.rb
@@ -28,10 +28,10 @@ module Librarian
           @module_path
         end
 
-        def system_cmd (cmd, print=@verbose)
-          puts "Running cmd: #{cmd}" if print
+        def system_cmd (cmd)
+          print_verbose "Running cmd: #{cmd}"
           output = `#{cmd}`.split("\n")
-          puts output.join("\n") if print
+          print_verbose output
           raise(StandardError, "Cmd #{cmd} failed") unless $?.success?
           output
         end
@@ -42,32 +42,38 @@ module Librarian
           #   puppetlabs/ntp  results in ntp 
           #   ntp             results in ntp 
           module_name = name.split('/', 2).last
-
           module_dir = File.join(module_path, module_name)
+
+          print_verbose "\n##### processing module #{name}..."
+
           case
           when options[:git]
             Dir.chdir(module_path) do
-              puts "cloning #{options[:git]}" if @verbose
+              print_verbose "cloning #{options[:git]}"
               system_cmd("git clone #{options[:git]} #{module_name}")
               Dir.chdir(module_dir) do
-                system_cmd('git branch -r') if @verbose
+                system_cmd('git branch -r')
                 if options[:ref]
                   system_cmd("git checkout #{options[:ref]}")
                 end
               end
             end
           when options[:tarball]
-            remote_target = options[:tarball]
+            remote_tarball = options[:tarball]
             Dir.mktmpdir do |tmp|
               local_target = File.join(tmp,"downloaded_module.tar.gz")
               File.open(local_target, "w+b") do |saved_file|
-                open(remote_target, 'rb') do |read_file|
+                print_verbose "Downloading #{remote_tarball}..."
+                open(remote_tarball, 'rb') do |read_file|
                   saved_file.write(read_file.read)
                 end
                 saved_file.rewind
+
+                target_directory = File.join(module_path, module_name)
+                print_verbose "Extracting #{remote_tarball} to #{target_directory}..."
                 unzipped_target = ungzip(saved_file)
                 tarfile_full_name = untar(unzipped_target, module_path)
-                FileUtils.mv File.join(module_path, tarfile_full_name), File.join(module_path, module_name)
+                FileUtils.mv File.join(module_path, tarfile_full_name), target_directory
               end
             end
           else
@@ -76,6 +82,10 @@ module Librarian
         end
 
         private
+
+        def print_verbose(text)
+          puts text if @verbose
+        end
 
         # un-gzips the given IO, returning the
         # decompressed version as a StringIO
